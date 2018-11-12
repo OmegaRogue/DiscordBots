4a47abeec77ebebc729efd7945f33b868e15bd92
OmegaRogue <OmegaRogue@users.noreply.github.com>
2018-11-12 19:25:13 +0100

    ijj

---
+++ MyCommands.cs
@@ -0,0 +1,139 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks; 
+using System.Collections.Concurrent;
+using System.Linq;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.CommandsNext.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
+
+namespace OmegaBot
+{
+    public class MyCommands
+    {
+        private ConcurrentDictionary<uint, Process> ffmpegs;
+        
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Author.Id == ctx.User.Id && xm.Content.ToLower() == "how are you?", TimeSpan.FromMinutes(1));
+            if (msg != null)
+                await ctx.RespondAsync($"I'm fine, thank you!");
+        }
+        
+        [Command("join")]
+        public async Task Join(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc != null)
+                throw new InvalidOperationException("Already connected in this guild.");
+
+            var chn = ctx.Member?.VoiceState?.Channel;
+            if (chn == null)
+                throw new InvalidOperationException("You need to be in a voice channel.");
+
+            vnc = await vnext.ConnectAsync(chn);
+            
+            this.ffmpegs = new ConcurrentDictionary<uint, Process>();
+            vnc.VoiceReceived += OnVoiceReceived;
+            
+            await ctx.RespondAsync("👌");
+        }
+
+        [Command("leave")]
+        public async Task Leave(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+                throw new InvalidOperationException("Not connected in this guild.");
+
+            vnc.VoiceReceived -= OnVoiceReceived;
+            foreach (var kvp in this.ffmpegs)
+            {
+                await kvp.Value.StandardInput.BaseStream.FlushAsync();
+                kvp.Value.StandardInput.Dispose();
+                kvp.Value.WaitForExit();
+            }
+            this.ffmpegs = null;
+            
+            vnc.Disconnect();
+            
+            await ctx.RespondAsync("👌");
+        }
+        
+        [Command("play")]
+        public async Task Play(CommandContext ctx, [RemainingText] string file)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+                throw new InvalidOperationException("Not connected in this guild.");
+
+            if (!File.Exists(file))
+                throw new FileNotFoundException("File was not found.");
+
+            await ctx.RespondAsync("👌");
+            await vnc.SendSpeakingAsync(true); // send a speaking indicator
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $@"-i ""{file}"" -ac 2 -f s16le -ar 48000 pipe:1",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            var ffmpeg = Process.Start(psi);
+            var ffout = ffmpeg.StandardOutput.BaseStream;
+
+            var buff = new byte[3840];
+            var br = 0;
+            while ((br = ffout.Read(buff, 0, buff.Length)) > 0)
+            {
+                if (br < buff.Length) // not a full sample, mute the rest
+                    for (var i = br; i < buff.Length; i++)
+                        buff[i] = 0;
+
+                await vnc.SendAsync(buff, 20);
+            }
+
+            await vnc.SendSpeakingAsync(false); // we're not speaking anymore
+        }
+        
+        public async Task OnVoiceReceived(VoiceReceiveEventArgs ea)
+        {
+            if (!this.ffmpegs.ContainsKey(ea.SSRC))
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $@"-ac 2 -f s16le -ar 48000 -i pipe:0 -ac 2 -ar 44100 {ea.SSRC}.wav",
+                    RedirectStandardInput = true
+                };
+
+                this.ffmpegs.TryAdd(ea.SSRC, Process.Start(psi));
+            }
+
+            var buff = ea.Voice.ToArray();
+
+            var ffmpeg = this.ffmpegs[ea.SSRC];
+            await ffmpeg.StandardInput.BaseStream.WriteAsync(buff, 0, buff.Length);
+            await ffmpeg.StandardInput.BaseStream.FlushAsync();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
 
 namespace OmegaBot
 {
     class Program
-    {
+    {   
+        private static DiscordClient discord;
+        static CommandsNextModule commands;
+        static InteractivityModule interactivity;
+        static VoiceNextClient voice;
+        static LavaLink
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
+        static async Task MainAsync(string[] args)
+        {
+            #if DEBUG
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "Mzc4ODY2MTIwNDAyNDAzMzI5.DsnGag.z3ZRSXMJClYTXrFNBL2deTVC-mM",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+            #else
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+            #endif
+            
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+            
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = ";;",
+                EnableDms = false
+            });
+            
+            commands.RegisterCommands<MyCommands>();
+            
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration());
+            
+            voice = discord.UseVoiceNext(new VoiceNextConfiguration
+            {
+                EnableIncoming = true
+            });
+
+
+            
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+            
     }
 }
